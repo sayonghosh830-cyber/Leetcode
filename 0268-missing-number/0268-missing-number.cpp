bfb82f97b9028fbe0e5db661e84a7d423ef30cba
Sayon Ghosh <sayonghosh830@gmail.com>
2026-07-12 13:19:49 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.72%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        while(i<n){
+            int correctIdx = nums[i];
+            if(correctIdx==i || nums[i]==n) i++;
+            else swap(nums[correctIdx],nums[i]);
+        }
+        for(int i = 0 ; i<n ; i++){
+            if(nums[i]!=i) return i;
+        }
+        return n;
+    }
+};
